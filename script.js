e234337c1b364d76eb10712087b8a5adb0bf5e53
Pavel <133679525+Mrpancho92@users.noreply.github.com>
2024-03-26 22:04:36 +0300

    Update 2024-03-26T19:04:29.529Z

--- script.js
+++ script.js
@@ -134,11 +134,42 @@ $(function () {
   });
 
   /* Collapse */
+  function offsetHeight($this) {
+    let heightText;
+    $(".accordion__content").map((item, index) => {
+      if ($this[0].children[1] === index) {
+        $this.children()[1].style.display = "block";
+        heightText = $this.children()[1].offsetHeight + 53.4 + "px";
+      }
+    });
+    return heightText;
+  }
+  $("[data-collapse]").each(function () {
+    if (!$(this).hasClass("active")) {
+      $(this)[0].style.height = "53.4px";
+    }
+    if ($(this).hasClass("active")) {
+      $(this)[0].style.height = offsetHeight($(this));
+    }
+  });
   $("[data-collapse]").on("click", function (event) {
     event.preventDefault();
-    let $this = $(this),
-      blockId = $this.data('collapse');
-    $this.toggleClass("active");
+    let $this = $(this);
+    if ($this.hasClass("active") === true) {
+      $(this).animate({
+        height: "53.4px"
+      }, 300, function () {
+        $this.children()[1].style.display = "none";
+        $this.toggleClass("active");
+      });
+    } else {
+      $(this).animate({
+        height: offsetHeight($this)
+      }, 300, function () {
+        $this.children()[1].style.display = "block";
+        $this.toggleClass("active");
+      });
+    }
   });
 
   /* Slider */
